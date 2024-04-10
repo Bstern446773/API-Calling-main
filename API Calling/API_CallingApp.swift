1e4557bef09e3de4bc6738980130104a35c74e3a
Bennett Stern <bstern@bsd220.org>
2024-04-10 09:35:13 -0500

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Bennett Stern on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
